68bb6e3888c3d6408ec3c0ed679cf3266fff1067
oracle <oracle@atanasoff32.danielcastelao.org>
2016-10-05 09:35:24 +0200

    Boletin2_10

---
+++ Boletin2_10/src/boletin2_10/Boletin2_10.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin2_10;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author JesusQuesada
+ */
+public class Boletin2_10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double billete100, billete20, billete5, moneda1, euros;
+        euros = Float.parseFloat(JOptionPane.showInputDialog("Total euros "));
+        billete100 = euros/100;
+        billete20 = euros/20;
+        billete5 = euros/5;
+        moneda1 = euros;
+        JOptionPane.showMessageDialog(null, "Billetes de 100 ="+billete100+"\nBilletes de 20 ="+billete20+"\nBilletes de 5 ="+billete5+"\nMonedas de 1 ="+moneda1);
+    }
+    
+}
