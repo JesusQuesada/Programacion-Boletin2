eb41f552f051dcaf23df449df98c4540363018cd
Jesus Quesada <jmqa82@gmail.com>
2016-10-06 09:25:29 +0200

    Update Boletin2_12.java

--- Boletin2_12/src/boletin2_12/Boletin2_12.java
+++ Boletin2_12/src/boletin2_12/Boletin2_12.java
@@ -17,14 +17,15 @@ public class Boletin2_12 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        float sueldofijo, ventas, km, dia_desp, sueldoliquido, ss, irpf;
+        float sueldofijo, ventas, km, dia_desp, totaldevengado, ss, irpf, sueldoliquido;
         sueldofijo = Float.parseFloat(JOptionPane.showInputDialog("Sueldo base "));
         ventas = Float.parseFloat(JOptionPane.showInputDialog("Ventas totales "));
         km = Float.parseFloat(JOptionPane.showInputDialog("Kilómetros totales "));
         dia_desp = Float.parseFloat(JOptionPane.showInputDialog("Días de desplazamiento "));
+        totaldevengado = sueldofijo+ventas*5/100+km*2+dia_desp*30;
         ss = 36;
-        irpf = 18/100;
-        sueldoliquido = sueldofijo+ventas*5/100+km*2+dia_desp*30-ss*irpf;
+        irpf = totaldevengado*18/100;
+        sueldoliquido = totaldevengado-ss-irpf;
         JOptionPane.showMessageDialog(null, "Sueldo líquido = "+sueldoliquido);
     }
     
