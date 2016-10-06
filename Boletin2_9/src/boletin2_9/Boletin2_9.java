6551ee80b0dd4d67bc124ad9722b651c4c4c73b9
Jesus Quesada <jmqa82@gmail.com>
2016-10-06 09:47:03 +0200

    Update Boletin2_9.java

--- Boletin2_9/src/boletin2_9/Boletin2_9.java
+++ Boletin2_9/src/boletin2_9/Boletin2_9.java
@@ -9,7 +9,7 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author jquesadaabeijon
+ * @author JesusQuesada
  */
 public class Boletin2_9 {
 
@@ -17,11 +17,11 @@ public class Boletin2_9 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        double billete100, billete20, billete5, moneda1, euros;
-        billete100 = Float.parseFloat(JOptionPane.showInputDialog("Billetes de 100 euros "));
-        billete20 = Float.parseFloat(JOptionPane.showInputDialog("Billetes de 20 euros "));
-        billete5 = Float.parseFloat(JOptionPane.showInputDialog("Billetes de 5 euros "));
-        moneda1 = Float.parseFloat(JOptionPane.showInputDialog("Monedas de 1 euro "));
+        int billete100, billete20, billete5, moneda1, euros;
+        billete100 = Integer.parseInt(JOptionPane.showInputDialog("Billetes de 100 euros "));
+        billete20 = Integer.parseInt(JOptionPane.showInputDialog("Billetes de 20 euros "));
+        billete5 = Integer.parseInt(JOptionPane.showInputDialog("Billetes de 5 euros "));
+        moneda1 = Integer.parseInt(JOptionPane.showInputDialog("Monedas de 1 euro "));
         euros = billete100*100+billete20*20+billete5*5+moneda1;
         JOptionPane.showMessageDialog(null, "Total euros "+euros);
     }
