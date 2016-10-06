bf10e06d8c2725dae2b56f696f0b2ff8f47102d6
Jesus Quesada <jmqa82@gmail.com>
2016-10-06 10:49:05 +0200

    Update Boletin2_7.java

--- Boletin2_7/src/boletin2_7/Boletin2_7.java
+++ Boletin2_7/src/boletin2_7/Boletin2_7.java
@@ -17,10 +17,12 @@ public class Boletin2_7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        double n, i, t, tm, im, rentamensual;
+        double n, rentamensual;
+        float i, tm, im;
+        int t;
         n = Double.parseDouble(JOptionPane.showInputDialog("Nominal "));
-        i = Double.parseDouble(JOptionPane.showInputDialog("Interés "));
-        t = Double.parseDouble(JOptionPane.showInputDialog("Años "));
+        i = Float.parseFloat(JOptionPane.showInputDialog("Interés "));
+        t = Integer.parseInt(JOptionPane.showInputDialog("Años "));
         tm = t*12;
         im = i/1200;
         rentamensual = n*Math.pow((1+im),tm)*im/(Math.pow((1+im),tm)-1);
