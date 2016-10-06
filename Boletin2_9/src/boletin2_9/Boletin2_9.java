c1fb4b85c970e4ed8ea31204d3eaffdf9725ba61
Jesus Quesada <jmqa82@gmail.com>
2016-10-06 11:08:32 +0200

    Update Boletin2_9.java

--- Boletin2_9/src/boletin2_9/Boletin2_9.java
+++ Boletin2_9/src/boletin2_9/Boletin2_9.java
@@ -17,11 +17,11 @@ public class Boletin2_9 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int billete100, billete20, billete5, moneda1, euros;
-        billete100 = Integer.parseInt(JOptionPane.showInputDialog("Billetes de 100 euros "));
-        billete20 = Integer.parseInt(JOptionPane.showInputDialog("Billetes de 20 euros "));
-        billete5 = Integer.parseInt(JOptionPane.showInputDialog("Billetes de 5 euros "));
-        moneda1 = Integer.parseInt(JOptionPane.showInputDialog("Monedas de 1 euro "));
+        long billete100, billete20, billete5, moneda1, euros;
+        billete100 = Long.parseLong(JOptionPane.showInputDialog("Billetes de 100 euros "));
+        billete20 = Long.parseLong(JOptionPane.showInputDialog("Billetes de 20 euros "));
+        billete5 = Long.parseLong(JOptionPane.showInputDialog("Billetes de 5 euros "));
+        moneda1 = Long.parseLong(JOptionPane.showInputDialog("Monedas de 1 euro "));
         euros = billete100*100+billete20*20+billete5*5+moneda1;
         JOptionPane.showMessageDialog(null, "Total euros "+euros);
     }
